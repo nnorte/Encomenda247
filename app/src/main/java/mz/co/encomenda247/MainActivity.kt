687ac671f96f1f79cfcec80c2ff6f9f69a54d469
nnorte <nnorte@ucm.ac.mz>
2020-10-17 20:18:23 +0200

    Initial commit

---
+++ app/src/main/java/mz/co/encomenda247/MainActivity.kt
@@ -0,0 +1,11 @@
+package mz.co.encomenda247
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
